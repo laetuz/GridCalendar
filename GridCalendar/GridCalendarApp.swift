405527a791415112739e2ae79ecb877492711f08
laetuz <laetuzg@gmail.com>
2023-10-16 13:14:57 +0700

    Initial Commit

---
+++ GridCalendar/GridCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridCalendarApp.swift
+//  GridCalendar
+//
+//  Created by Ryo Martin on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GridCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
